43b93493305bca9c131b640fb9aeb82caa30ba12
rafailsohbatov <sohbatov.rafail@gmail.com>
2023-07-31 15:34:02 +0400

    added Payment Class

---
+++ src/az/mycompany/course/service/PaymentServiceImpl.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package az.mycompany.course.service;
+
+import az.mycompany.course.dao.PaymentDao;
+import az.mycompany.course.model.Payment;
+import java.util.List;
+
+/**
+ *
+ * @author Admin
+ */
+public class PaymentServiceImpl implements PaymentService {
+
+    private PaymentDao paymentDao;
+
+    public PaymentServiceImpl(PaymentDao paymentDao) {
+        this.paymentDao = paymentDao;
+    }
+
+    @Override
+    public List<Payment> getPaymentList() throws Exception {
+        return paymentDao.getPaymentList();
+    }
+
+}
